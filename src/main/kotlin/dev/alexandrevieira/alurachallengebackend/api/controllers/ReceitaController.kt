4e84c06bf3ce1a187df746464adff7597d24f92c
vieira-alexandre <alexandrer0x@hotmail.com>
2022-01-23 19:53:16 -0300

    Adiciona listagem e busca de receitas

--- src/main/kotlin/dev/alexandrevieira/alurachallengebackend/api/controllers/ReceitaController.kt
+++ src/main/kotlin/dev/alexandrevieira/alurachallengebackend/api/controllers/ReceitaController.kt
@@ -2,8 +2,12 @@ package dev.alexandrevieira.alurachallengebackend.api.controllers
 
 import dev.alexandrevieira.alurachallengebackend.api.ReceitaApi
 import dev.alexandrevieira.alurachallengebackend.api.dto.request.NovaReceitaRequest
+import dev.alexandrevieira.alurachallengebackend.api.dto.response.ReceitaResponse
+import dev.alexandrevieira.alurachallengebackend.exception.NotFoundException
 import dev.alexandrevieira.alurachallengebackend.model.entities.Receita
 import dev.alexandrevieira.alurachallengebackend.model.repositories.ReceitaRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
@@ -18,5 +22,15 @@ class ReceitaController(
         repository.save(receita)
         val uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(receita.id).toUri()
         return ResponseEntity.created(uri).build()
+    }
+
+    override fun listar(pageable: Pageable): Page<ReceitaResponse> {
+        return repository.findAll(pageable).map { ReceitaResponse.of(it) }
+    }
+
+    override fun detalhar(id: Long): ReceitaResponse {
+        val receitaOptional = repository.findById(id)
+        if (receitaOptional.isEmpty) throw NotFoundException(Receita::class)
+        return ReceitaResponse.of(receitaOptional.get())
     }
 }
