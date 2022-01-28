ab1c0f57c0d0f15a4cd0d534ff81ac8a13fa2df8
vieira-alexandre <alexandrer0x@hotmail.com>
2022-01-27 23:04:07 -0300

    Adiciona camada de serviço para receitas

---
+++ src/main/kotlin/dev/alexandrevieira/alurachallengebackend/service/ReceitaService.kt
@@ -0,0 +1,50 @@
+package dev.alexandrevieira.alurachallengebackend.service
+
+import dev.alexandrevieira.alurachallengebackend.exception.NotFoundException
+import dev.alexandrevieira.alurachallengebackend.exception.UnprocessableEntityException
+import dev.alexandrevieira.alurachallengebackend.model.entities.Receita
+import dev.alexandrevieira.alurachallengebackend.model.repositories.ReceitaRepository
+import dev.alexandrevieira.alurachallengebackend.util.Mensagens
+import org.springframework.beans.BeanUtils
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class ReceitaService(
+    val repository: ReceitaRepository,
+) {
+    fun cadastrar(receita: Receita): Receita {
+        return repository.save(receita)
+    }
+
+    fun listar(pageable: Pageable): Page<Receita> {
+        return repository.findAll(pageable)
+    }
+
+    fun detalhar(id: Long): Receita {
+        return repository.findById(id).orElseThrow { throw NotFoundException(Receita::class) }
+    }
+
+    fun excluir(id: Long) {
+        if (repository.existsById(id)) repository.deleteById(id)
+        else throw NotFoundException(Receita::class)
+    }
+
+    fun atualizar(id: Long, request: Receita) {
+        val receitaOptional: Optional<Receita> = repository.findById(id)
+        if (receitaOptional.isEmpty) throw NotFoundException(Receita::class)
+        val carregada: Receita = receitaOptional.get()
+
+        if (estaAlterando(carregada, request) && repository.existsByDescricaoAndMes(request.descricao, request.mes))
+            throw UnprocessableEntityException(Mensagens.receitaJaCadastrada)
+
+        BeanUtils.copyProperties(request, receitaOptional.get(), "id")
+        repository.save(receitaOptional.get())
+    }
+
+    private fun estaAlterando(receita1: Receita, receita2: Receita): Boolean {
+        return receita1.mes != receita2.mes || receita1.descricao != receita2.descricao
+    }
+}
--- src/main/kotlin/dev/alexandrevieira/alurachallengebackend/util/Mensagens.kt
+++ src/main/kotlin/dev/alexandrevieira/alurachallengebackend/util/Mensagens.kt
@@ -2,4 +2,5 @@ package dev.alexandrevieira.alurachallengebackend.util
 
 object Mensagens {
     const val despesaJaCadastrada = "Despesa já cadastrada para o mês informado"
+    const val receitaJaCadastrada = "Receita já cadastrada para o mês informado"
 }
